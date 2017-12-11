970a488e2c15be45edf1b5ab054ccde937e04ab9
cuihaichen@macpro <519376470@qq.com>
2017-12-11 18:57:31 +0800

    状态恢复文章开篇

--- app/src/main/java/com/example/activitylifecycle/MainActivity.java
+++ app/src/main/java/com/example/activitylifecycle/MainActivity.java
@@ -18,6 +18,22 @@ import android.view.View;
  * 2)在当前Activity开启另一个Activity，此回调在后一个Activity的onResume执行完成后执行
  * 3)锁屏或home键退出app
  * 在onPause()之后，onStop()之前执行
+ *
+ * 1.需求介绍
+ *  公司项目的某个业务，需要在整个业务操作流程中保存一些数据，我们采用了单例模式来保存这些数据，然后在
+ *  整个业务流程中随时取用。正常操作的情况下是没有问题的，但是在bugly异常上报中，出现了很多在后台拉起App
+ *  时，出现空指针Crash的问题。经检查，发现是App被异常杀死后，重新创建App时单例对象中的数据为空导致的。
+ * 1.什么是Activity的状态恢复？
+ *  Activity提供了相应的保存状态和恢复状态的回调方法。保存状态使用的是onSaveInstanceState(Bundle)，同时
+ *  还提供了另一个方法onSaveInstanceState(Bundle,PersistableBundle)。恢复状态使用的是onCreate(Bundle)
+ *  、onRestoreInstanceState(Bundle)以及onRestoreInstanceState(Bundle,PersistableBundle)。
+ *      通过上述方法，在Activity销毁时保存状态，在需要重建时恢复状态，从而保证全局数据的恢复。
+ * 2.怎么实现状态恢复？
+ *  1) 方法简介
+ *  2）方法生命周期
+ *  3）保存和重置状态
+ * 3.为什么要使用这种方式？
+ * 4.这种方式有什么好处，又有什么局限？
  */
 
 public class MainActivity extends AppCompatActivity {
@@ -45,6 +61,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
         Log.i("xiong", "onRestoreInstanceState()");
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onRestoreInstanceState(savedInstanceState, persistentState);
     }
 
     @Override
